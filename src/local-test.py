9b9e3c5218acbaef7be20c1c203ec8090ae38239
Jack Tseng <jack.w.tseng@outlook.com>
2022-12-21 13:22:47 +0800

    update

--- src/local-test.py
+++ src/local-test.py
@@ -20,12 +20,13 @@ driver = webdriver.Remote(
 # Browser setting
 driver.set_window_size(width=1366, height=768)
 driver.delete_all_cookies()
-driver.implicitly_wait(35)
+driver.implicitly_wait(15)
 
 # Run
 logging.info('Run')
 driver.get("https://devalto.ruckuswireless.com")
 
+# login
 # user name
 driver.find_element_by_id('user_username').send_keys(
     'dog1051@email.com')
@@ -36,11 +37,18 @@ driver.find_element_by_id('user_password').send_keys('password-1')
 # login
 driver.find_element_by_css_selector(
     'input[type="submit"]').click()
-
-# Check result
 logging.info('Sleep')
-sleep(60)
+sleep(15)
 
-# End
-logging.info('End')
-driver.quit()
+# dashboard
+try:
+    logging.info('Try')
+    driver.find_element_by_link_text('Venues')
+except:
+    logging.info('Except')
+    driver.refresh()
+finally:
+    logging.info('Finally')
+    for data in driver.get_log('browser'):
+        logging.info(data)
+    driver.quit()
---
+++ src/remote-test.py
@@ -0,0 +1,58 @@
+from selenium import webdriver
+from time import sleep
+import logging
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.common.by import By
+
+# Loggin
+logging.basicConfig(level=logging.DEBUG)
+
+# Setup capabilities
+logging.info('Set capabilities')
+capabilities = webdriver.DesiredCapabilities.EDGE
+#capabilities = webdriver.DesiredCapabilities.CHROME
+logging.debug(capabilities)
+
+# Start
+logging.info('Start')
+driver = webdriver.Remote(
+    command_executor='http://192.168.2.46:32019/wd/hub',
+    desired_capabilities=capabilities
+)
+
+# Browser setting
+driver.set_window_size(width=1366, height=768)
+driver.delete_all_cookies()
+driver.implicitly_wait(35)
+
+# Run
+logging.info('Run')
+driver.get("https://devalto.ruckuswireless.com")
+
+# login
+# user name
+driver.find_element_by_id('user_username').send_keys(
+    'dog1051@email.com')
+
+# user password
+driver.find_element_by_id('user_password').send_keys('password-1')
+
+# login
+driver.find_element_by_css_selector(
+    'input[type="submit"]').click()
+logging.info('Sleep')
+sleep(15)
+
+# dashboard
+try:
+    logging.info('Try')
+    driver.find_element_by_link_text('Venues')
+except:
+    logging.info('Except')
+    driver.refresh()
+finally:
+    logging.info('Finally') 
+    for data in driver.get_log('browser'):
+        logging.info(data)
+    driver.quit()
