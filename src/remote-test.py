89033552859777a38a338498162f62eac7820d22
Jack Tseng <jack.w.tseng@outlook.com>
2022-12-22 14:59:31 +0800

    update

--- src/remote-test.py
+++ src/remote-test.py
@@ -6,66 +6,67 @@ from selenium.webdriver.common.by import By
 from time import sleep
 import logging
 
-# Loggin
-logging.basicConfig(level=logging.INFO)
 
-# Start
-logging.info('Start')
-driver = webdriver.Remote(
-    command_executor='http://192.168.2.46:31623/wd/hub',
-    desired_capabilities=webdriver.DesiredCapabilities.FIREFOX
-)
+def login(login_url, user_name, user_password):
+    logging.info('Login')
+    driver.get(login_url)
 
-# Browser setting
-driver.set_window_size(width=1920, height=1080)
-driver.delete_all_cookies()
-driver.implicitly_wait(15)
+    # User name
+    driver.find_element_by_id('user_username').send_keys(user_name)
 
-# Run
-logging.info('Run')
-driver.get("https://qaalto.ruckuswireless.com")
+    # User password
+    driver.find_element_by_id('user_password').send_keys(user_password)
 
-# login
-# user name
-driver.find_element_by_id('user_username').send_keys(
-    'dog1051@email.com')
+    # Submit
+    driver.find_element_by_css_selector('input[type="submit"]').click()
 
-# user password
-driver.find_element_by_id('user_password').send_keys('password-1')
 
-# login
-driver.find_element_by_css_selector(
-    'input[type="submit"]').click()
+if __name__ == '__main__':
+    # Logging
+    logging.basicConfig(level=logging.INFO)
 
-# dashboard
-# leftMenuItems = driver.find_elements_by_css_selector('rc-left-menu nav a div')
+    # Start
+    logging.info('Start')
+    driver = webdriver.Remote(
+        command_executor='http://192.168.2.46:31623/wd/hub',
+        desired_capabilities=webdriver.DesiredCapabilities.FIREFOX
+    )
 
-try:
-    logging.info('Try')
-    venuesButton = WebDriverWait(driver, 30).until(
-        expected_conditions.element_to_be_clickable((By.LINK_TEXT, 'Venues')))
-    # venuesButton = driver.find_element_by_link_text('Venues')
+    # Browser setting
+    driver.set_window_size(width=1920, height=1080)
+    driver.delete_all_cookies()
+    driver.implicitly_wait(15)
 
-    if venuesButton:
-        logging.info('Click')
-        venuesButton.click()
-        sleep(30)
-    else:
-        logging.info('No venuesButton')
+    # Log in
+    login("https://qaalto.ruckuswireless.com",
+          "dog1051@email.com", "password-1")
 
-except exceptions.NoSuchElementException:
-    logging.warning('NoSuchElementException')
+    try:
+        logging.info('Try')
+        venuesButton = WebDriverWait(driver, 30).until(
+            expected_conditions.element_to_be_clickable((By.LINK_TEXT, 'Venues')))
+        # venuesButton = driver.find_element_by_link_text('Venues')
 
-except exceptions.TimeoutException:
-    logging.warning('TimeoutException')
+        if venuesButton:
+            logging.info('Click')
+            venuesButton.click()
+            sleep(30)
+        else:
+            logging.info('No venuesButton')
 
-except AttributeError:
-    logging.warning('AttributeError')
+    except exceptions.NoSuchElementException:
+        logging.warning('NoSuchElementException')
 
-# End task
-driver.close()
+    except exceptions.TimeoutException:
+        logging.warning('TimeoutException')
 
-try:
-    driver.quit()
-except exceptions.InvalidSessionIdException as err:
-    logging.warn(err.msg)
+    except AttributeError:
+        logging.warning('AttributeError')
+
+    # End task
+    driver.close()
+
+    try:
+        driver.quit()
+    except exceptions.InvalidSessionIdException as err:
+        logging.warn(err.msg)
