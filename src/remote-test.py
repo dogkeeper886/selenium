5e059a2178857048a235ed42a572fc54323791db
Jack Tseng <jack.w.tseng@outlook.com>
2022-12-27 16:25:06 +0800

    update

--- src/remote-test.py
+++ src/remote-test.py
@@ -5,10 +5,36 @@ from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.common.by import By
 from time import sleep
 import logging
+import sys
 
 
+def driverDecorator(func):
+    def wrapper(*args, **kwargs):
+        try:
+            logging.info(func.__name__)
+            func(*args, **kwargs)
+
+        except exceptions.NoSuchElementException:
+            logging.warning('Except %s: NoSuchElementException', func.__name__)
+            endTask()
+            sys.exit()
+
+        except exceptions.TimeoutException:
+            logging.warning('Except %s: TimeoutException', func.__name__)
+            endTask()
+            sys.exit()
+
+        except exceptions.InvalidSessionIdException as err:
+            logging.warning('Except %s: %s', func.__name__, err.msg)
+
+        finally:
+            sleep(5)
+
+    return wrapper
+
+
+@driverDecorator
 def login(login_url, user_name, user_password):
-    logging.info('Login')
     driver.get(login_url)
 
     # User name
@@ -19,6 +45,42 @@ def login(login_url, user_name, user_password):
 
     # Submit
     driver.find_element_by_css_selector('input[type="submit"]').click()
+
+
+@driverDecorator
+def endTask():
+    driver.close()
+    driver.quit()
+
+
+@driverDecorator
+def leftMenu(itemName):
+    menuButton = WebDriverWait(driver, 10).until(
+        expected_conditions.element_to_be_clickable((By.LINK_TEXT, itemName))
+    )
+    menuButton.click()
+
+
+def textElementFinder(elementText, css):
+    elementList = driver.find_elements_by_css_selector(css)
+    for element in elementList:
+        if element.text == elementText:
+            return element
+    else:
+        logging.info('No element match %s', elementText)
+
+
+@driverDecorator
+def venue(venueName):
+    venueTextElement = textElementFinder(venueName, 'span')
+    venueTextElement.click()
+
+@driverDecorator
+def settings(buttonName):
+    dropdownButton = textElementFinder('Settings', 'span')
+    dropdownButton.click()
+    button = textElementFinder(buttonName, 'span')
+    button.click()
 
 
 if __name__ == '__main__':
@@ -35,38 +97,23 @@ if __name__ == '__main__':
     # Browser setting
     driver.set_window_size(width=1920, height=1080)
     driver.delete_all_cookies()
-    driver.implicitly_wait(15)
+    driver.implicitly_wait(10)
 
     # Log in
-    login("https://qaalto.ruckuswireless.com",
-          "dog1051@email.com", "password-1")
+    login(
+        'https://qaalto.ruckuswireless.com',
+        'dog1051@email.com',
+        'password-1'
+    )
 
-    try:
-        logging.info('Try')
-        venuesButton = WebDriverWait(driver, 30).until(
-            expected_conditions.element_to_be_clickable((By.LINK_TEXT, 'Venues')))
-        # venuesButton = driver.find_element_by_link_text('Venues')
-
-        if venuesButton:
-            logging.info('Click')
-            venuesButton.click()
-            sleep(30)
-        else:
-            logging.info('No venuesButton')
-
-    except exceptions.NoSuchElementException:
-        logging.warning('NoSuchElementException')
-
-    except exceptions.TimeoutException:
-        logging.warning('TimeoutException')
-
-    except AttributeError:
-        logging.warning('AttributeError')
+    # Go to Venues
+    leftMenu('Venues')
+    venue('My-Venue')
+    #settings('Wi-Fi Settings')
+    settings('Switch Settings')
+    
+    # Test
+    sleep(30)
 
     # End task
-    driver.close()
-
-    try:
-        driver.quit()
-    except exceptions.InvalidSessionIdException as err:
-        logging.warn(err.msg)
+    endTask()
